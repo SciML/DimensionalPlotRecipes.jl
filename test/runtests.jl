7658c840b9069a809745c439ed584e3614c18e71
MRIDUL JAIN <105979087+Spinachboul@users.noreply.github.com>
2024-01-27 15:42:15 +0530

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,7 @@ plot(t,A)
 
 t = range(0,stop=1,length=5)
 plot(t,A)
+plot(t,A,transformation=:split2D)
 plot(t,A,transformation=:split3D)
 plot(t,A,transformation=:modulus)
 plot(t,A,transformation=:modulus2)
